d05a843d23e7ca22cf0d9d4edfb48fd4eaabe932
Sameer Tele <sameertele@Sameers-MacBook-Pro.local>
2024-08-14 19:34:19 -0500

    Resolved merge conflicts

--- athena_queries/create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS enron_spam_data (
-  subject string,
-  message string,
-  label int
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES (
-  'serialization.format' = '1'
-) LOCATION 's3://enron-spam-detection-data/processed/';
